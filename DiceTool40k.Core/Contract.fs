74ee6d7ae898ac8cc7fd97d64ed59967f7389519
Kristian Jørgensen <kristjorge@gmail.com>
2024-11-29 19:48:21 +0100

    [Core] Add dto layer

---
+++ DiceTool40k.Core/Contract.fs
@@ -0,0 +1,43 @@
+module DiceTool40K.Contract
+
+type SustainedHitsDto =
+    { Constant: int
+      Variable: string option }
+
+type AttacksDto =
+    { Constant: int
+      Variable: string option }
+
+type SpecialRulesDto =
+    { LetalHits: bool
+      DevestatingWounds: bool
+      SustainedHits: SustainedHitsDto
+      Blast: bool }
+
+type DamageTypeDto =
+    { Constant: int
+      Variable: string option }
+
+type AttackingModelDto =
+    { NumAttacks: AttacksDto
+      Skill: int
+      ArmourPiercing: int
+      Strength: int
+      Damage: DamageTypeDto
+      SpecialRules: SpecialRulesDto
+      ToHitModifier: int option
+      ToWoundModifier: int option
+      CritHitDiceValue: int option
+      CritWoundDiceValue: int option
+      ReRollHit: string option
+      ReRollWounds: string option }
+
+type DefendingModelsDto =
+    { Toughness: int
+      Save: int
+      Invul: int option
+      Wounds: int
+      DamageModifier: string option
+      ToHitModifier: int option
+      ToWoundModifier: int option
+      NumModels: int }
